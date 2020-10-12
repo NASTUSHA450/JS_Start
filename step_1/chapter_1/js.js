32f008655d0afa20b348e1ac6838921836eb0929
Nastya <nastyadragunskaya@gmail.com>
2020-10-12 16:53:44 +0300

    new project

---
+++ step_1/chapter_1/js.js
@@ -0,0 +1,14 @@
+alert('Долго будешь смотреть на эту унылую страницу');
+// variable
+/*mnogostrochni*/
+/**
+ * 
+ * @param {num1} x 
+ * @param {num2} n 
+ */
+function pow(x, n){
+    var res = x*n;
+    return res;
+}
+pow(4,5);                                                    
+
