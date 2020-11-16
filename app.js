6d558a72122f3b9e5f053a200ee3848ed368b745
Nastya <nastyadragunskaya@gmail.com>
2020-11-16 11:17:16 +0200

    got a pug)

--- app.js
+++ app.js
@@ -4,23 +4,27 @@ let express = require('express');
 //создаём новый обьект  экземпляр класса express
 let app = express();
 
-let port = 3000;
-
+const port = 3000;
+const framework = 'express';
+//%s строка переноситься
 app.listen(port, function(){
-    console.log(`node express work on ${port}`);
+    console.log(`node %s work on %s`,framework,port);
 });
 
 //public -имя папки где храниться статика
 app.use(express.static('public'));
 
+//прописываем путь до папки
+app.set('views', './views');
+//запуск шаблонизатора
+app.set('view engine','pug');
+
 //Далее, чтобы запустить первое приложение
 //get - метод обращения, первый параметр url адрес куда идет обратно
 
 app.get('/',function(req,res){
-    res.render('index.html');
+    res.render('main');
 });
 
-app.get('/cat',function(req,res){
-    res.render('category.html');
-});
+
 
